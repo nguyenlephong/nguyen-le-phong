07f5a2c044730e97c6b2143fcc5c3c37468996e2
Nguyễn Lê Phong <nguyenlephong1997@gmail.com>
2024-04-05 21:47:06 +0700

    chore: remove error boundary

--- src/problem2/fancy-form/src/routes/AppRoute.tsx
+++ src/problem2/fancy-form/src/routes/AppRoute.tsx
@@ -1,13 +1,11 @@
 import {createBrowserRouter, createRoutesFromElements, Route,} from "react-router-dom";
 import SwapPage from "../pages/swap/Swap.page";
-import ErrorRouteBoundary from "../components/ErrorRouteBoundary";
 import {APP_ROUTE} from "configs/app.const";
 
 export const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route
       path={APP_ROUTE.HOME}
-      errorElement={<ErrorRouteBoundary/>}
     >
       <Route path={APP_ROUTE.SWAP} element={<SwapPage/>}/>
     </Route>
